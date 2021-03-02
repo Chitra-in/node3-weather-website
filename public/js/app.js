85cba745c06e65df063d8a7eda8ca88bf8f5ca88
unknown <kiddyguys2005@gmail.com>
2021-03-02 19:37:37 +0530

    Remove header printing to console

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-console.log('CLient side JAva script code is loaded')
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne= document.querySelector('#message-1')
